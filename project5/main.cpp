e0e98e17ca3808330f466361446c71fea90f8ff9
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-09-24 21:27:05 -0600

    adding in initial test for isDivisable()

---
+++ project5/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+bool isDivisible(int n, int target){
+	for(int i = 1; i <= n; i++) {
+		if(target%i != 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+	cout << isDivisable(10, 2520) << endl;
+	return 0;
+}
