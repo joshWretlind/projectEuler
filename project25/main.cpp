23b44faefd28373ab23f700e65cc318c0ba00b81
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-09-24 21:55:31 -0600

    adding in project25

---
+++ project25/main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+//this is an array of fibonacci numbers
+vector<long double> fibonacci;
+
+/**
+ * caclulateFibonacci
+ * Purpose: This is to calculate all of the fibonacci numbers below 4 million
+ * Complexity: Time:  O(N)
+ *             Space: O(N)
+ **/
+int calculateFibonacci(){
+	fibonacci.push_back(1.0L);
+	fibonacci.push_back(1.0L);
+	int i;
+	for(i = 2; fibonacci[i-2] + fibonacci[i-1] < pow(10.0L,1000.0L); i++) {
+		fibonacci.push_back(fibonacci[i-2] + fibonacci[i-1]);
+	}
+	return i;
+}
+
+int main(){
+	cout << calculateFibonacci() << endl;
+}
