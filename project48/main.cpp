d20111e242b3bc07abc5d2bcd12893b36fc2c909
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-09-24 22:24:51 -0600

    adding in initial test for problem 28

---
+++ project48/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+long double calculateSelfPowers(int size){
+	long double count = 0;
+	for(int i = 1; i <= size; i++){
+		count += pow(i,i);
+	}
+}
+
+int main(){
+	cout << calculateSelfPowers(10) << endl; 
+}
